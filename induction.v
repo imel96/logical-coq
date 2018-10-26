0659788050faaeebd3e89f2f1cc6afc5249ac39c
Melby Sjamsuddin <melbys@benon.com>
2018-10-26 16:09:43 +1000

    Add induction
    as above

--- induction.v
+++ induction.v
@@ -147,3 +147,90 @@ reflexivity.
 simpl.
 reflexivity.
 Qed.
+
+Theorem mult_0_r : forall n:nat,
+  n * 0 = 0.
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+apply IHn.
+Qed.
+
+Theorem plus_n_Sm : forall n m : nat,
+  S (n + m) = n + (S m).
+Proof.
+intros.
+induction n.
+simpl.
+reflexivity.
+simpl.
+rewrite <- IHn.
+reflexivity.
+Qed.
+
+Theorem plus_n_O : forall n:nat, n = n + 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite <- IHn'. reflexivity.
+Qed.
+
+Theorem plus_comm : forall n m : nat,
+  n + m = m + n.
+Proof.
+intros.
+induction n as [|n'].
+rewrite <- plus_n_O.
+reflexivity.
+rewrite <- plus_n_Sm.
+rewrite <- IHn'.
+simpl.
+reflexivity.
+Qed.
+
+Theorem plus_assoc : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+intros.
+induction n as [|n'].
+simpl.
+rewrite plus_comm.
+reflexivity.
+simpl.
+rewrite IHn'.
+reflexivity.
+Qed.
+
+Fixpoint double (n:nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n .
+Proof.
+intros.
+induction n as [|n'].
+simpl.
+reflexivity.
+simpl.
+rewrite <- plus_comm.
+rewrite IHn'.
+simpl.
+reflexivity.
+Qed.
+
+Fixpoint evenb (n:nat) : bool :=
+  match n with
+  | O => true
+  | S O => false
+  | S (S n') => evenb n'
+  end.
+
+Theorem evenb_S : forall n : nat,
+  evenb (S n) = negb (evenb n).
+Proof.
+intros.
+
