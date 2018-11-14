c049629900fbd619ac4955d9b255f883565ad8bd
Melby Sjamsuddin <melbys@benon.com>
2018-11-14 17:27:14 +1000

    countoddmembers
    ^^

--- lists.v
+++ lists.v
@@ -272,4 +272,116 @@ rewrite H.
 reflexivity.
 Qed.
 
-Fixpoint nonzeros (l:natlist) : natlist
+Theorem negation_fn_applied_twice:
+  forall (f : bool -> bool), (forall (x : bool), f x = negb x) -> forall (b : bool), f (f b) = b.
+Proof.
+intros.
+rewrite -> H.
+rewrite -> H.
+destruct b.
+simpl.
+reflexivity.
+simpl.
+reflexivity.
+Qed.
+
+Fixpoint evenb (n:nat) : bool :=
+  match n with
+  | O => true
+  | S O => false
+  | S (S n') => evenb n'
+  end.
+
+Theorem evenb_S : forall n : nat,
+  evenb (S n) = negb (evenb n).
+Proof.
+intros.
+induction n as [|n'].
+simpl.
+reflexivity.
+rewrite IHn'.
+simpl.
+rewrite negation_fn_applied_twice.
+reflexivity.
+intros.
+reflexivity.
+Qed.
+
+Fixpoint length (l:natlist) : nat :=
+  match l with
+  | nil => O
+  | h :: t => S (length t)
+  end.
+
+Fixpoint nonzeros (l:natlist) : natlist :=
+  match l with
+    | nil => nil
+    | 0 :: tl => nonzeros(tl)
+    | h::tl => h::nonzeros(tl)
+end.
+
+Example test_nonzeros:
+  nonzeros [0;1;0;2;3;0;0] = [1;2;3].
+Proof.
+simpl.
+reflexivity.
+Qed.
+Fixpoint oddmembers (l:natlist) : natlist :=
+  match l with
+    | nil => nil
+    | h::tl => match evenb(h) with
+      | true => oddmembers(tl)
+      | false =>  h::oddmembers(tl)
+  end
+end.
+
+Example test_oddmembers:
+  oddmembers [0;1;0;2;3;0;0] = [1;3].
+Proof.
+simpl.
+reflexivity.
+Qed.
+
+Definition countoddmembers (l:natlist) : nat :=
+ length(oddmembers l).
+
+Example test_countoddmembers1:
+  countoddmembers [1;0;3;1;4;5] = 4.
+Proof.
+reflexivity.
+Qed.
+
+Example test_countoddmembers2:
+  countoddmembers [0;2;4] = 0.
+Proof.
+reflexivity.
+Qed.
+
+Example test_countoddmembers3:
+  countoddmembers nil = 0.
+Proof.
+reflexivity.
+Qed.
+
+Fixpoint beq_natlist (l1 l2 : natlist) : bool :=
+  match l1 with
+  | nil => match l2 with
+    | nil => true
+    | l => false
+  end
+  | l => match l2 with
+    | nil => false
+    | l => false
+   end
+end.
+
+Example test_beq_natlist1 :
+  (beq_natlist nil nil = true).
+Proof.
+reflexivity.
+Qed.
+
+Example test_beq_natlist2 :
+  beq_natlist [1;2;3] [1;2;3] = true.
+Proof.
+simpl.
