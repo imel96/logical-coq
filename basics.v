6700ab330092e5043daf5cd468bbba38ad5b664a
Melby Sjamsuddin <melbysjamsuddin@darksilver.local>
2019-09-18 17:41:03 +1000

    Definition ltb
    ^^

--- basics.v
+++ basics.v
@@ -25,6 +25,52 @@ Fixpoint factorial (n: nat) : nat :=
     | S n => S n * factorial n
   end.
 
+Fixpoint eqb (n m : nat) : bool :=
+  match n with
+  | O => match m with
+         | O => true
+         | S m' => false
+         end
+  | S n' => match m with
+            | O => false
+            | S m' => eqb n' m'
+            end
+  end.
+
+Fixpoint leb (n m : nat) : bool :=
+  match n with
+  | O => true
+  | S n' =>
+      match m with
+      | O => false
+      | S m' => leb n' m'
+      end
+  end.
+
+Notation "x =? y" := (eqb x y) (at level 70) : nat_scope.
+Notation "x <=? y" := (leb x y) (at level 70) : nat_scope.
+
+Definition ltb (n m : nat) : bool :=
+	match n <=? m with
+	| false => false
+	| true => negb(n =? m)
+	end.
+
+Example test_ltb1: (ltb 2 2) = false.
+Proof.
+reflexivity.
+Qed.
+
+Example test_ltb2: (ltb 2 4) = true.
+Proof.
+reflexivity.
+Qed.
+
+Example test_ltb3: (ltb 4 2) = false.
+Proof.
+reflexivity.
+Qed.
+
 Theorem plus_id_exercise : forall n m o : nat,
   n = m -> m = o -> n + m = m + o.
 Proof.
